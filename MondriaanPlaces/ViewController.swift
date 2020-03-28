275af42aee0ac81811e9c184560b8714f5d0e870
Benedek Varga <vargabenedek92@gmail.com>
2020-03-28 10:43:38 +0100

    Initial Commit

---
+++ MondriaanPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MondriaanPlaces
+//
+//  Created by Benedek Varga on 2020. 03. 28..
+//  Copyright © 2020. Benedek Varga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
